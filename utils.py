bfab6afa93d037d680271a8b1db0e908bddb2d93
Noah-Vincenz Noeh <NoahVincenz.Noeh@aexp.com>
2020-09-29 10:14:40 +0100

    Removed spaces in comments.

--- utils.py
+++ utils.py
@@ -82,6 +82,7 @@ def generate_risikokennzahlen_vola_query(aktie):
     '''.format(aktie)
 
 KUMULATIV_QUERIES = [
+    "SELECT datum, valor FROM `user_aktkurse_kurse` WHERE (datum = LAST_DAY(date_sub(LAST_DAY(date_sub(now(),interval 1 month)),interval 0 year)) AND datum > '2008-12-01') OR (datum = LAST_DAY(date_sub(LAST_DAY(date_sub(now(),interval 1 month)),interval 1 month)) AND datum > '2008-12-01') ORDER BY datum ASC",
     generate_kumulativ_query("1 month"),
     generate_kumulativ_query("3 month"),
     generate_kumulativ_query("6 month"),
@@ -91,30 +92,24 @@ KUMULATIV_QUERIES = [
     generate_kumulativ_query("5 year"),
     generate_kumulativ_query("10 year")
 ]
-KUMULATIV_SHEET = 'kumulativ'
+KUMULATIV_SHEET = 'kumulative Eträge'
 KUMULATIV_TABLE_NAMES = [
-    "Abfrage kumulativ 1 Monat",
-    "Abfrage kumulativ 3 Monate",
-    "Abfrage kumulativ 6 Monate",
-    "Abfrage kumulativ 1 Jahr",
-    "Abfrage kumulativ 2 Jahre",
-    "Abfrage kumulativ 3 Jahre",
-    "Abfrage kumulativ 5 Jahre",
-    "Abfrage kumulativ 10 Jahre"
-]
-DIESER_MONAT_QUERIES = [
-    "SELECT datum, valor FROM `user_aktkurse_kurse` WHERE (datum = LAST_DAY(date_sub(LAST_DAY(date_sub(now(),interval 1 month)),interval 0 year)) AND datum > '2008-12-01') OR (datum = LAST_DAY(date_sub(LAST_DAY(date_sub(now(),interval 1 month)),interval 1 month)) AND datum > '2008-12-01') ORDER BY datum ASC"
-]
-DIESER_MONAT_SHEET = 'dieser_monat'
-DIESER_MONAT_TABLE_NAMES = [
-    "Abfrage dieser Monat"
+    "Dieser Monat",
+    "1 Monat",
+    "3 Monate",
+    "6 Monate",
+    "1 Jahr",
+    "2 Jahre",
+    "3 Jahre",
+    "5 Jahre",
+    "10 Jahre"
 ]
 RISIKOKENNZAHLEN_VOLA_QUERIES = [
     generate_risikokennzahlen_vola_query("CRB"),
     generate_risikokennzahlen_vola_query("Euribor3m"),
     generate_risikokennzahlen_vola_query("Valor")
 ]
-RISIKOKENNZAHLEN_VOLA_SHEET = 'risikokennzahlen_vola'
+RISIKOKENNZAHLEN_VOLA_SHEET = 'Risikokennzahlen'
 RISIKOKENNZAHLEN_VOLA_TABLE_NAMES = [
     "Abfrage Risikokennzahlen Vola CRB",
     "Abfrage Risikokennzahlen Vola Euribor",
@@ -123,7 +118,7 @@ RISIKOKENNZAHLEN_VOLA_TABLE_NAMES = [
 VALOR_QUERIES = [
     "SELECT datum, valor FROM `user_aktkurse_kurse` WHERE (datum = LAST_DAY(date_sub(LAST_DAY(date_sub(now(),interval 1 month)),interval 0 year)) AND datum > '2008-11-15') OR (datum = LAST_DAY(date_sub(LAST_DAY(date_sub(now(),interval 1 month)),interval 1 year)) AND datum > '2008-11-15') OR (datum = LAST_DAY(date_sub(LAST_DAY(date_sub(now(),interval 1 month)),interval 2 year)) AND datum > '2008-11-15') OR (datum = LAST_DAY(date_sub(LAST_DAY(date_sub(now(),interval 1 month)),interval 3 year)) AND datum > '2008-11-15') OR (datum = LAST_DAY(date_sub(LAST_DAY(date_sub(now(),interval 1 month)),interval 4 year)) AND datum > '2008-11-15') OR (datum = LAST_DAY(date_sub(LAST_DAY(date_sub(now(),interval 1 month)),interval 5 year)) AND datum > '2008-11-15') ORDER BY datum ASC"
 ]
-VALOR_SHEET = 'valor'
+VALOR_SHEET = 'Valor'
 VALOR_TABLE_NAMES = ["Abfrage Valor"]
 HISTORISCHE_WERTENTWICKLUNG_QUERIES = [
     "SELECT user_aktkurse_kurse_0.uid, user_aktkurse_kurse_0.pid, user_aktkurse_kurse_0.tstamp, user_aktkurse_kurse_0.crdate, user_aktkurse_kurse_0.cruser_id, user_aktkurse_kurse_0.sorting, user_aktkurse_kurse_0.deleted, user_aktkurse_kurse_0.hidden,  user_aktkurse_kurse_0.valor,  user_aktkurse_kurse_0.CRB, user_aktkurse_kurse_0.datum FROM maisondo_kurse.user_aktkurse_kurse user_aktkurse_kurse_0"
--- xls_writer.py
+++ xls_writer.py
@@ -4,7 +4,7 @@ import pandas as pd
 from dotenv import load_dotenv
 from datetime import datetime
 from writer import Writer
-from utils import KUMULATIV_QUERIES, KUMULATIV_SHEET, KUMULATIV_TABLE_NAMES, DIESER_MONAT_QUERIES, DIESER_MONAT_SHEET, DIESER_MONAT_TABLE_NAMES, RISIKOKENNZAHLEN_VOLA_QUERIES, RISIKOKENNZAHLEN_VOLA_SHEET, RISIKOKENNZAHLEN_VOLA_TABLE_NAMES, VALOR_QUERIES, VALOR_SHEET, VALOR_TABLE_NAMES, HISTORISCHE_WERTENTWICKLUNG_QUERIES, HISTORISCHE_WERTENTWICKLUNG_SHEET, HISTORISCHE_WERTENTWICKLUNG_TABLE_NAMES
+from utils import KUMULATIV_QUERIES, KUMULATIV_SHEET, KUMULATIV_TABLE_NAMES, RISIKOKENNZAHLEN_VOLA_QUERIES, RISIKOKENNZAHLEN_VOLA_SHEET, RISIKOKENNZAHLEN_VOLA_TABLE_NAMES, VALOR_QUERIES, VALOR_SHEET, VALOR_TABLE_NAMES, HISTORISCHE_WERTENTWICKLUNG_QUERIES, HISTORISCHE_WERTENTWICKLUNG_SHEET, HISTORISCHE_WERTENTWICKLUNG_TABLE_NAMES
 
 load_dotenv()
 
@@ -18,20 +18,18 @@ print("Connected to:", db.get_server_info())
 
 cursor = db.cursor()
 today = datetime.today().date()
-# Create Pandas Excel writer using XlsxWriter engine
+#Create Pandas Excel writer using XlsxWriter engine
 pandas_writer = pd.ExcelWriter('daten_{}.xlsx'.format(today), engine='xlsxwriter', options={'strings_to_numbers': True})
-# Create custom Writer class
+#Create custom Writer class
 excel_writer = Writer(cursor, pandas_writer)
 
-# KUMULATIV
-excel_writer.execute_queries(KUMULATIV_QUERIES, ['Datum', 'Valor'], KUMULATIV_SHEET, KUMULATIV_TABLE_NAMES)
-# DIESER MONAT
-excel_writer.execute_queries(DIESER_MONAT_QUERIES, ['Datum', 'Valor'], DIESER_MONAT_SHEET, DIESER_MONAT_TABLE_NAMES)
-# RISIKOKENNZAHLEN VOLA
+#KUMULATIV
+excel_writer.execute_queries(KUMULATIV_QUERIES, ['Datum', 'Summe von valor'], KUMULATIV_SHEET, KUMULATIV_TABLE_NAMES)
+#RISIKOKENNZAHLEN VOLA
 excel_writer.execute_queries_with_plot(RISIKOKENNZAHLEN_VOLA_QUERIES, ['Datum', 'Wert'], RISIKOKENNZAHLEN_VOLA_SHEET, RISIKOKENNZAHLEN_VOLA_TABLE_NAMES)
-# VALOR
-excel_writer.execute_queries_with_plot(VALOR_QUERIES, ['Datum', 'Valor'], VALOR_SHEET, VALOR_TABLE_NAMES)
-# HISTORISCHE WERTENTWICKLUNG
+#VALOR
+excel_writer.execute_queries_with_plot(VALOR_QUERIES, ['Datum', 'Summe von valor'], VALOR_SHEET, VALOR_TABLE_NAMES)
+#HISTORISCHE WERTENTWICKLUNG
 excel_writer.execute_queries(HISTORISCHE_WERTENTWICKLUNG_QUERIES, ['uid', 'pid', 'tstamp', 'crdate', 'cruser_id', 'sorting', 'deleted', 'hidden', 'valor', 'crb', 'datum'], HISTORISCHE_WERTENTWICKLUNG_SHEET, HISTORISCHE_WERTENTWICKLUNG_TABLE_NAMES)
-# Close the writer
+#Close the writer
 pandas_writer.save()
