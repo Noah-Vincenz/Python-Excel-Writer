d9af3b0a6d2ffc629bf669853d6699c3fbf87172
Noah-Vincenz Noeh <NoahVincenz.Noeh@aexp.com>
2020-10-02 11:29:30 +0100

    Updated code to update existing excel file instead of creating new one.

--- xls_writer.py
+++ xls_writer.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from datetime import datetime
 from writer import Writer
 from utils import KUMULATIV_QUERIES, KUMULATIV_SHEET, KUMULATIV_TABLE_NAMES, RISIKOKENNZAHLEN_VOLA_QUERIES, RISIKOKENNZAHLEN_VOLA_SHEET, RISIKOKENNZAHLEN_VOLA_TABLE_NAMES, VALOR_QUERIES, VALOR_SHEET, VALOR_TABLE_NAMES, HISTORISCHE_WERTENTWICKLUNG_QUERIES, HISTORISCHE_WERTENTWICKLUNG_SHEET, HISTORISCHE_WERTENTWICKLUNG_TABLE_NAMES
+from openpyxl import load_workbook
 
 load_dotenv()
 
@@ -14,12 +15,14 @@ db = mysql.connect(
   password=os.getenv('PASSWORD'),
   database=os.getenv('DATABASE')
 )
-print("Connected to:", db.get_server_info())
-
 cursor = db.cursor()
 today = datetime.today().date()
+# TODO: copy file into output folder and rename with datum as suffix
 #Create Pandas Excel writer using XlsxWriter engine
-pandas_writer = pd.ExcelWriter('daten_{}.xlsx'.format(today), engine='xlsxwriter', options={'strings_to_numbers': True})
+path = 'valor.xlsx'
+book = load_workbook(path)
+pandas_writer = pd.ExcelWriter(path, engine = 'openpyxl', options={'strings_to_numbers': True})
+pandas_writer.book = book
 #Create custom Writer class
 excel_writer = Writer(cursor, pandas_writer)
 
@@ -33,3 +36,4 @@ excel_writer.execute_queries_with_plot(VALOR_QUERIES, ['Datum', 'Summe von valor
 excel_writer.execute_queries_with_plot(HISTORISCHE_WERTENTWICKLUNG_QUERIES, ['Datum', 'Valor'], HISTORISCHE_WERTENTWICKLUNG_SHEET, HISTORISCHE_WERTENTWICKLUNG_TABLE_NAMES)
 #Close the writer
 pandas_writer.save()
+pandas_writer.close()
